287475fc4894f5d0e1c9c8b297a03f7930e79d79
Elostay <nastia60203@gmail.com>
2023-12-10 11:17:59 +0200

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,20 +5,15 @@ import Filter from './Filter/Filter';
 import ContactList from './ContactList/ContactList';
 import { Title, SubTitle, Container } from './ContactForm/ContactForm.styled';
 
-import { persistor } from '../redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
-
 const App = () => {
   return (
-    <PersistGate loading={null} persistor={persistor}>
-      <Container>
-        <Title>Phonebook</Title>
-        <ContactForm />
-        <SubTitle>Contacts</SubTitle>
-        <Filter />
-        <ContactList />
-      </Container>
-    </PersistGate>
+    <Container>
+      <Title>Phonebook</Title>
+      <ContactForm />
+      <SubTitle>Contacts</SubTitle>
+      <Filter />
+      <ContactList />
+    </Container>
   );
 };
 
